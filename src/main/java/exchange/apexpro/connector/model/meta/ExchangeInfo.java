ae2c1299c7446a01774e780b209a60f5d0917476
nik <>
2023-11-24 16:27:13 +0000

    SKEW | Fixed compilation issues.

--- src/main/java/exchange/apexpro/connector/model/meta/ExchangeInfo.java
+++ src/main/java/exchange/apexpro/connector/model/meta/ExchangeInfo.java
@@ -11,7 +11,6 @@ import exchange.apexpro.connector.constant.ApiConstants;
 import exchange.apexpro.connector.enums.ApexSupportedMarket;
 import exchange.apexpro.connector.exception.ApexProApiException;
 import exchange.apexpro.connector.impl.utils.json.CostBigDecimalAdapter;
-import exchange.apexpro.connector.impl.utils.json.CustomLongDecimalAdapter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
